a062d681c2817bb65eb9b9a554dea4cbe5741257
Swen Mulderij <swenmulderij@gmail.com>
2018-06-04 15:45:53 +0200

    Relationshop and mutations started

--- schema.py
+++ schema.py
@@ -3,19 +3,27 @@ from graphene import ObjectType, Schema
 import records
 from helpers import get_single_result
 from graphql import GraphQLError
+from validators import *
+
 
 # Thema objects
 
-
-class Thema(ObjectType):
+class Thema(graphene.ObjectType):
     id = graphene.ID()
     naam = graphene.String(description="Naam van het thema")
     beschrijving = graphene.String(description="Beschrijving van het thema")
+    opgaven = graphene.List(
+        lambda: Opgave, description="Opgaven die bij dit thema horen")
+
+    def resolve_opgaven(self, info, **kwards):
+        db = records.Database("sqlite:///mock.db")
+        results = db.query('select * from opgaven where thema=:id', id=self.id)
+        return results
 
 
-class ThemaQueries(ObjectType):
+class ThemaQueries(graphene.ObjectType):
     themas = graphene.List(Thema)
-    thema = graphene.Field(Thema, id=graphene.Int(required=True))
+    thema = graphene.Field(lambda: Thema, id=graphene.Int(required=True))
 
     def resolve_thema(root, info, **kwargs):
         id = kwargs.get('id')
@@ -23,17 +31,45 @@ class ThemaQueries(ObjectType):
         db = records.Database("sqlite:///mock.db")
         results = db.query('select * from themas where id=:id',
                            id=id)
-        return results.first(default=GraphQLError(f'Thema met id {id} is niet gevonden'))
+        return results.first(
+            default=GraphQLError(f'Thema met id {id} is niet gevonden'))
 
     def resolve_themas(root, info):
         db = records.Database("sqlite:///mock.db")
         results = db.query('select * from themas')
         return results
 
+
+class ThemaInput(graphene.InputObjectType):
+    naam = graphene.String()
+    beschrijving = graphene.String()
+    opgaven = graphene.List(graphene.ID)
+
+
+class CreateThema(graphene.Mutation):
+    class Arguments():
+        thema_data = ThemaInput()
+
+    ok = graphene.Boolean()
+    thema = graphene.Field(lambda: Thema)
+
+    def mutate(self, info, thema_data=None):
+        if themaValidator.validate(thema_data):
+            db = records.Database("sqlite:///mock.db")
+            db.query(
+                '''INSERT INTO themas (naam, beschrijving) 
+                VALUES (:naam, :beschrijving)''',
+                naam=thema_data.naam, beschrijving=thema_data.beschrijving)
+            result = db.query('SELECT last_insert_rowid()')
+            print(list(result))
+            return CreateThema(ok=True, thema=None)
+        else:
+            raise GraphQLError(themaValidator.errors)
+
+
 # Opgave objects
 
-
-class Opgave(ObjectType):
+class Opgave(graphene.ObjectType):
     id = graphene.ID()
     naam = graphene.String(description="Naam van de opgave")
     beschrijving = graphene.String(description="Beschrijving van de opgave")
@@ -42,13 +78,12 @@ class Opgave(ObjectType):
 
     def resolve_thema(self, info, **kwargs):
         db = records.Database("sqlite:///mock.db")
-        # print(self.thema)
         results = db.query('select * from themas where id=:id',
                            id=self.thema)
         return results.first()
 
 
-class OpgaveQueries(ObjectType):
+class OpgaveQueries(graphene.ObjectType):
     opgaven = graphene.List(Opgave)
     opgave = graphene.Field(Opgave, id=graphene.Int(required=True))
 
@@ -58,12 +93,22 @@ class OpgaveQueries(ObjectType):
         db = records.Database("sqlite:///mock.db")
         results = db.query('select * from opgaven where id=:id',
                            id=id)
-        return results.first(default=GraphQLError(f'Opgave met id {id} is niet gevonden'))
+        single_result = results.first()
+        if single_result:
+            return single_result
+        else:
+            raise GraphQLError(f'Opgave met id {id} is niet gevonden')
 
     def resolve_opgaven(root, info):
         db = records.Database("sqlite:///mock.db")
         results = db.query('select * from opgaven')
         return results
+
+# Mutation queries
+
+
+class Mutations(graphene.ObjectType):
+    create_thema = CreateThema.Field()
 
 # Hoofdquery
 
@@ -73,4 +118,4 @@ class Query(ThemaQueries, OpgaveQueries):
         name = "Root query"
 
 
-Schema = Schema(query=Query)
+Schema = Schema(query=Query, mutation=Mutations)
