63934f671a73b116e7fb1bc88625e93e00575e06
Swen Mulderij <swenmulderij@gmail.com>
2018-06-18 14:04:28 +0200

    proper server settings

--- settings.py
+++ settings.py
@@ -3,8 +3,8 @@ import records
 # Database settings
 
 DB_USER = 'sa'
-DB_PASS = 'Zu!dHoll@nd'
-DB_HOST = 'localhost'
+DB_PASS = 'sa_PRODPoCPZH123!'
+DB_HOST = 'PRODPoCPZH'
 DB_PORT = '1433'
 DB_NAME = 'PZH_db'
-DB_DRIVER = 'FreeTDS'
+DB_DRIVER = 'SQL Server Native Client 11.0'
