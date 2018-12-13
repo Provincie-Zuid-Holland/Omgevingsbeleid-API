a46906639bc3231c1e2ad57e4fe1968ec926c5da
Swen Mulderij <swenmulderij@gmail.com>
2018-12-13 14:40:47 +0100

    Saaie dimensies nieuwe stijl

---
+++ Dimensies/themas.py
@@ -0,0 +1,6 @@
+import marshmallow as MM
+from .dimensie import Dimensie_Schema
+
+class Themas_Schema(Dimensie_Schema):
+    Titel = MM.fields.Str(required=True)
+    Omschrijving = MM.fields.Str(missing=None)
--- server.py
+++ server.py
@@ -13,8 +13,17 @@ from Dimensies.ambitie import Ambitie_Schema
 from Dimensies.beleidsregel import BeleidsRegel_Schema
 from Dimensies.doel import Doel_Schema
 from Dimensies.provincialebelangen import ProvincialeBelangen_Schema
+from Dimensies.beleidsrelaties import BeleidsRelatie_Schema
+from Dimensies.maatregelen import Maatregelen_Schema
+from Dimensies.themas import Themas_Schema
+from Dimensies.opgaven import Opgaven_Schema
+from Dimensies.verordening import Verordening_Schema
 
-# from Feiten.beleidsbeslissing import BeleidsBeslissing
+from Feiten.beleidsbeslissing import BeleidsBeslissing
+
+from Dimensies.geothemas import Geothema
+from Dimensies.gebruikers import Gebruiker
+from Dimensies.werkingsgebieden import Werkingsgebied
 
 from Auth.views import login
 from Auth.commands import new_client_creds
@@ -56,27 +65,33 @@ api.add_resource(Dimensie, '/doelen', '/doelen/<string:uuid>', endpoint='Doelen'
 api.add_resource(Dimensie, '/beleidsregels', '/beleidsregels/<string:uuid>', endpoint='Beleidsregels',
                  resource_class_args=(BeleidsRegel_Schema, 'BeleidsRegels', 'Actuele_BeleidsRegels'))                 
 
-api.add_resource(Dimensie, '/provincialebelangen',
-                 '/provincialebelangen/<string:uuid>', endpoint='Provincialebelangen',
+api.add_resource(Dimensie, '/provincialebelangen', '/provincialebelangen/<string:uuid>', endpoint='Provincialebelangen',
                  resource_class_args=(ProvincialeBelangen_Schema, 'ProvinicialeBelangen', 'Actuele_ProvincialeBelangen'))
-# api.add_resource(Thema, '/themas',
-#                  '/themas/<string:thema_uuid>')
-# api.add_resource(Opgave, '/opgaven',
-#                  '/opgaven/<string:opgave_uuid>')
-# api.add_resource(Maatregel, '/maatregelen',
-#                  '/maatregelen/<string:maatregel_uuid>')
-# api.add_resource(Verordening, '/verordeningen',
-#                  '/verordeningen/<string:verordening_uuid>')
-# api.add_resource(Werkingsgebied, '/werkingsgebieden',
-#                  '/werkingsgebieden/<string:werkingsgebied_uuid>')
-# api.add_resource(Geothema, '/geothemas',
-#                  '/geothemas/<string:geothema_uuid>')
-# api.add_resource(BeleidsRelatie, '/beleidsrelaties',
-#                  '/beleidsrelaties/<string:beleidsrelatie_uuid>')
-# api.add_resource(Gebruiker, '/gebruikers',
-#                  '/gebruikers/<string:gebruiker_uuid>')
-# api.add_resource(BeleidsBeslissing, '/beleidsbeslissingen',
-#                  '/beleidsbeslissingen/<string:beleidsbeslissing_uuid>')
+
+api.add_resource(Dimensie, '/beleidsrelaties', '/beleidsrelaties/<string:uuid>', endpoint='Beleidsrelaties',
+                 resource_class_args=(BeleidsRelatie_Schema, 'BeleidsRelaties', 'Actuele_BeleidsRelaties'))
+
+api.add_resource(Dimensie, '/maatregelen', '/maatregelen/<string:uuid>', endpoint='Maatregelen', 
+                 resource_class_args=(Maatregelen_Schema, 'Maatregelen', 'Actuele_Maatregelen'))
+
+api.add_resource(Dimensie, '/themas', '/themas/<string:uuid>', endpoint='Themas', 
+                 resource_class_args=(Themas_Schema, 'Themas', 'Actuele_Themas'))                 
+
+api.add_resource(Dimensie, '/opgaven', '/opgaven/<string:uuid>', endpoint='Opgaven', 
+                 resource_class_args=(Opgaven_Schema, 'Opgaven', 'Actuele_Opgaven'))                 
+
+api.add_resource(Dimensie, '/verordeningen', '/verordeningen/<string:uuid>', endpoint='Verordeningen',
+                 resource_class_args=(Verordening_Schema, 'Verordeningen', 'Actuele_Verordeningen'))
+
+api.add_resource(Werkingsgebied, '/werkingsgebieden',
+                 '/werkingsgebieden/<string:werkingsgebied_uuid>')
+api.add_resource(Geothema, '/geothemas',
+                 '/geothemas/<string:geothema_uuid>')
+api.add_resource(Gebruiker, '/gebruikers',
+                 '/gebruikers/<string:gebruiker_uuid>')
+
+api.add_resource(BeleidsBeslissing, '/beleidsbeslissingen',
+                 '/beleidsbeslissingen/<string:beleidsbeslissing_uuid>')
 
 if __name__ == '__main__':
     app.run()
