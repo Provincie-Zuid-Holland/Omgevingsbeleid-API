5a558fa2c26e349808894254a0aad7c1068f511c
Swen Mulderij <swenmulderij@gmail.com>
2019-11-12 16:47:53 +0100

    XML endpoint post/get

--- Special/verordeningsstructuur.py
+++ Special/verordeningsstructuur.py
@@ -1,3 +1,4 @@
+import datetime
 import marshmallow as MM
 from flask import request, jsonify
 from flask_restful import Resource
@@ -43,12 +44,11 @@ def serialize_schema_to_xml(schema):
     Turn a Verordening_Scructuur_Schema into a xml string
     """
     tree = ET.Element('tree', {'xmlns:xs': 'http://www.w3.org/2001/XMLSchema', 'xmlns': 'Verordening_Tree'})
-    # xml = ET.ElementTree(tree)
     uuid = ET.SubElement(tree, 'uuid')
     uuid.text = str(schema['UUID'])
     for child in schema['Children']:
         tree.append(_serialize_child_to_xml(child))
-    return ET.tostring(tree, encoding='utf8', method='xml').decode()
+    return ET.tostring(tree, encoding='unicode', method='xml')
 
 
 def _serialize_child_to_xml(childschema):
@@ -99,14 +99,17 @@ def row_to_dict(row):
 
 def handle_odbc_exception(odbc_ex):
     pattern = re.compile(r'FK_\w+_(\w+)')
-    match = pattern.search(e.args[-1]).group(1)
+    match = pattern.search(odbc_ex.args[-1])
     if match:
+        match = match.group(1)
         return {'message': f'Database integriteitsfout, een identifier naar een "{match}" object is niet geldig'}, 404
     code = odbc_ex.args[0]
     return {"message": f"Database error [{code}] during handling of request", "detailed": str(odbc_ex)}, 500
 
 def ob_auto_filter(field):
     return field.metadata.get('ob_auto', False)
+
+
 
 class Verordening_Structuur(Resource):
 
@@ -161,12 +164,53 @@ class Verordening_Structuur(Resource):
         return results
 
     def post(self):
+        if request.get_json() is None:
+            return {'message': 'Request data empty'}, 400
+        
         try:
             excluded = map(lambda k: k[0], filter(lambda k: ob_auto_filter(k[1]), Verordening_Structuur_Schema().fields.items()))
             read_schema = Verordening_Structuur_Schema(exclude=excluded, unknown=MM.RAISE)
-            parsed = read_schema.load(request.get_json())
+            vo_object = read_schema.load(request.get_json())
+        
         except MM.exceptions.ValidationError as err:
             return err.normalized_messages(), 400
-        if 'Structuur' in parsed:
-            parsed['Structuur'] = serialize_schema_to_xml(parsed['Structuur'])
-        return jsonify(parsed)
+        
+        old_struct = vo_object['Structuur']
+
+        if 'Structuur' in vo_object:
+            vo_object['Structuur'] = serialize_schema_to_xml(vo_object['Structuur'])
+        
+        # Add missing data
+        vo_object['Created_By'] = get_jwt_identity()['UUID']
+        vo_object['Created_Date'] = datetime.datetime.now()
+        vo_object['Modified_Date'] = vo_object['Created_Date']
+        vo_object['Modified_By'] = vo_object['Created_By']
+
+        keys, values = list(zip(*vo_object.items()))
+        argmarks = ("? ," * len(keys))[:-2]
+
+        create_query = f'''
+        SET NOCOUNT ON
+        DECLARE @generated_identifiers table ([uuid] uniqueidentifier, [id] int)
+
+        INSERT INTO [VerordeningStructuur] ({', '.join(keys)}) OUTPUT inserted.UUID, inserted.ID into @generated_identifiers VALUES ({argmarks})
+
+        SELECT uuid, id from @generated_identifiers
+        '''
+
+        with pyodbc.connect(db_connection_settings) as connection:
+            try:
+                cursor = connection.cursor()
+                cursor.execute(create_query, *values)
+                outputted = cursor.fetchone()
+                uuid = outputted[0]
+                id = outputted[1]
+            except pyodbc.Error as e:
+                return handle_odbc_exception(e), 500
+
+        vo_object['Structuur'] = old_struct
+        vo_object['UUID'] = uuid
+        vo_object['ID'] = id
+
+
+        return Verordening_Structuur_Schema().dump(vo_object), 200
