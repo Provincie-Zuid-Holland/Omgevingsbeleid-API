6ba11bf8bc87bc21ce8a49955f76767e9cdde4b6
Swen Mulderij <swenmulderij@gmail.com>
2020-01-21 14:51:45 +0100

    removed settings.py

--- settings.py
+++
@@ -1,8 +0,0 @@
-# Database settings
-
-DB_USER = 'api_dev'
-DB_PASS = '#D3v-C0mpl3x!'
-DB_HOST = 'db-svr-omgevingsbeleid.database.windows.net'
-DB_PORT = '1433'
-DB_NAME = 'db-dev-omgevingsbeleid'
-DB_DRIVER = 'FreeTDS'
