6fd218eb511d4c3ed2fea46cf8864f445769a837
Swen Mulderij <swenmulderij@gmail.com>
2020-02-04 13:12:47 +0100

    removed constraints

--- Dimensies/verordening.py
+++ Dimensies/verordening.py
@@ -1,15 +1,16 @@
 import marshmallow as MM
 from .dimensie import Dimensie_Schema
+from globals import default_user_uuid
 
 class Verordening_Schema(Dimensie_Schema):
     Titel = MM.fields.Str(required=True, search_field="text")
     Inhoud = MM.fields.Str(missing=None, search_field="text")
     Status = MM.fields.Str(required=True)
-    Type = MM.fields.Str(required=True, validate= [MM.validate.OneOf(['Hoofdstuk', 'Afdeling', 'Paragraaf', 'Artikel']),])
+    Type = MM.fields.Str(required=True, validate=[MM.validate.OneOf(['Hoofdstuk', 'Afdeling', 'Paragraaf', 'Artikel']), ])
     Volgnummer = MM.fields.Str(required=True)
-    Werkingsgebied = MM.fields.UUID(missing=None, attribute= 'fk_Werkingsgebied')
-    Eigenaar_1 = MM.fields.UUID(required=False)
-    Eigenaar_2 = MM.fields.UUID(required=False)
-    Portefeuillehouder_1 = MM.fields.UUID(required=False)
-    Portefeuillehouder_2 = MM.fields.UUID(required=False)
-    Opdrachtgever = MM.fields.UUID(required=False)
+    Werkingsgebied = MM.fields.UUID(missing=None, attribute='fk_Werkingsgebied')
+    Eigenaar_1 = MM.fields.UUID(required=False, missing=default_user_uuid)
+    Eigenaar_2 = MM.fields.UUID(required=False, missing=default_user_uuid)
+    Portefeuillehouder_1 = MM.fields.UUID(required=False, missing=default_user_uuid)
+    Portefeuillehouder_2 = MM.fields.UUID(required=False, missing=default_user_uuid)
+    Opdrachtgever = MM.fields.UUID(required=False, missing=default_user_uuid)
