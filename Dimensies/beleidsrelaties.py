09fb3eafaf943bd6b88c16f96bb3458542a7bc53
Mulderij <sj.mulderij@pzh.nl>
2020-02-18 14:48:27 +0100

    New status type

--- Dimensies/beleidsrelaties.py
+++ Dimensies/beleidsrelaties.py
@@ -7,6 +7,6 @@ class BeleidsRelatie_Schema(Dimensie_Schema):
     Naar_Beleidsbeslissing = MM.fields.UUID(required=True)
     Titel = MM.fields.Str(required=True, search_field="text")
     Omschrijving = MM.fields.Str(missing=None, search_field="text")
-    Status = MM.fields.Str(required=True, validate=[MM.validate.OneOf(['Open', 'Akkoord', 'NietAkkoord'])])
+    Status = MM.fields.Str(required=True, validate=[MM.validate.OneOf(['Open', 'Akkoord', 'NietAkkoord', 'Afgewezen'])])
     Aanvraag_Datum = MM.fields.DateTime(format='iso', required=True)
     Datum_Akkoord = MM.fields.DateTime(format='iso', allow_none=True, missing=None)
