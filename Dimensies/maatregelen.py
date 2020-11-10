b3964ac2a54d1d805033ab9ed670f9c268b98b98
Swendude <swenmulderij@gmail.com>
2020-11-10 11:19:59 +0100

    Onnodige velden weggehaald.

--- Dimensies/maatregelen.py
+++ Dimensies/maatregelen.py
@@ -10,15 +10,12 @@ import re
 
 class Maatregelen_Schema(Dimensie_Schema):
     Titel = MM.fields.Str(required=True, obprops=['search_field'])
-    # Omschrijving = MM.fields.Str(missing=None, obprops=['search_field'])
     Toelichting = MM.fields.Str(missing=None, obprops=[])
     Toelichting_Raw = MM.fields.Method(missing=None, obprops=['search_field'])
     Gebied = MM.fields.UUID(
         missing=None, attribute='fk_Gebied', obprops=['geo_field'])
     Gebied_Duiding = MM.fields.Str(allow_none=True, missing="Indicatief", validate=[
                                    MM.validate.OneOf(["Indicatief", "Exact"])], obprops=[])
-    # Verplicht_Programma = MM.fields.Str(missing=None, validate= [MM.validate.OneOf(["Ja", "Nee"])], obprops=[])
-    # Specifiek_Of_Generiek = MM.fields.Str(missing=None, validate= [MM.validate.OneOf(["Gebiedsspecifiek", "Generiek"])], obprops=[])
     Weblink = MM.fields.Str(missing=None, obprops=[])
     Tags = MM.fields.Str(missing=None, obprops=[])
     Status = MM.fields.Str(required=True, validate=[MM.validate.OneOf([
