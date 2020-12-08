a59928a5cb2a3002c1ffb4ab2fd2b449e99cf435
Swendude <swenmulderij@gmail.com>
2020-12-08 15:52:33 +0100

    Import fix.

--- Dimensies/ambitie.py
+++ Dimensies/ambitie.py
@@ -1,13 +1,13 @@
 import marshmallow as MM
-from .dimensie import Dimensie_Schema
+from Endpoints.normal import Normal_Schema
 
 
-class Ambitie_Schema(Dimensie_Schema):
+class Ambitie_Schema(Normal_Schema):
     Titel = MM.fields.Str(required=True, obprops=['search_field'])
     Omschrijving = MM.fields.Str(missing=None, obprops=['search_field'])
     Weblink = MM.fields.Str(missing=None, obprops=[])
 
-    class Meta(Dimensie_Schema.Meta):
+    class Meta(Normal_Schema.Meta):
         table= 'Ambities'
         read_only = False
         ordered = True
