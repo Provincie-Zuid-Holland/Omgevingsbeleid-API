9029627dfa3bb816ec9e6bd3dae58d3f90663688
Swendude <swenmulderij@gmail.com>
2021-03-02 14:20:38 +0100

    parent aae06374311adae8e3e24c66af7ab4f6fcb1c28c author Swendude <swenmulderij@gmail.com> 1614691238 +0100 committer Swendude <swenmulderij@gmail.com> 1615546026 +0100
    Basic HTML Validation.

--- Models/beleidsrelaties.py
+++ Models/beleidsrelaties.py
@@ -3,19 +3,26 @@
 
 import marshmallow as MM
 from Endpoints.endpoint import Base_Schema
-from Endpoints.references import UUID_Reference 
+from Endpoints.references import UUID_Reference
+from Endpoints.validators import HTML_Validate
 from Models.beleidskeuzes import Beleidskeuzes_Schema
 from globals import null_uuid
 
+
 class Beleidsrelaties_Schema(Base_Schema):
-    Van_Beleidskeuze = MM.fields.UUID(required=True, allow_none=False, validate=MM.validate.NoneOf([null_uuid,]), obprops=[])
-    Naar_Beleidskeuze = MM.fields.UUID(required=True, allow_none=False, validate=MM.validate.NoneOf([null_uuid,]), obprops=[])
-    Titel = MM.fields.Str(required=True, obprops=['search_field'])
-    Omschrijving = MM.fields.Str(missing=None, obprops=['search_field'])
-    Status = MM.fields.Str(required=True, validate=MM.validate.OneOf(['Open', 'Akkoord', 'NietAkkoord', 'Verbroken']), obprops=[])
-    Aanvraag_Datum = MM.fields.DateTime(format='iso', required=True, obprops=[])
-    Datum_Akkoord = MM.fields.DateTime(format='iso', allow_none=True, missing=None, obprops=[])
-    
+    Van_Beleidskeuze = MM.fields.UUID(
+        required=True, allow_none=False, validate=MM.validate.NoneOf([null_uuid, ]), obprops=[])
+    Naar_Beleidskeuze = MM.fields.UUID(
+        required=True, allow_none=False, validate=MM.validate.NoneOf([null_uuid, ]), obprops=[])
+    Titel = MM.fields.Str(required=True, validate=[HTML_Validate], obprops=['search_field'])
+    Omschrijving = MM.fields.Str(missing=None, validate=[HTML_Validate], obprops=['search_field'])
+    Status = MM.fields.Str(required=True, validate=MM.validate.OneOf(
+        ['Open', 'Akkoord', 'NietAkkoord', 'Verbroken']), obprops=[])
+    Aanvraag_Datum = MM.fields.DateTime(
+        format='iso', required=True, obprops=[])
+    Datum_Akkoord = MM.fields.DateTime(
+        format='iso', allow_none=True, missing=None, obprops=[])
+
     class Meta(Base_Schema.Meta):
         slug = 'beleidsrelaties'
         table = 'Beleidsrelaties'
@@ -27,4 +34,3 @@ class Beleidsrelaties_Schema(Base_Schema):
             'Naar_Beleidskeuze': UUID_Reference('Beleidskeuzes', Beleidskeuzes_Schema),
 
         }
-    
--- Tests/test_data.py
+++ Tests/test_data.py
@@ -3,36 +3,41 @@
 
 import marshmallow as MM
 from globals import null_uuid
-from Endpoints.custom_fields import UUID_ReferenceList
 
-def generate_data(schema, user_UUID=null_uuid, excluded_prop = None):
+
+def generate_data(schema, user_UUID=null_uuid, excluded_prop=None):
     fields = schema(exclude=schema.fields_with_props(excluded_prop)).fields
     result = {}
     for field in fields:
-        if fields[field].validate:
-            try:
-                result[field] = fields[field].validate.choices[0]
-            except AttributeError:
-                continue   
-        
-        elif field == 'Created_By' or field == 'Modified_By':
+        validatee = False
+        if fields[field].validators:
+            validators = fields[field].validators
+            for validator in validators:
+                if isinstance(validator, MM.validate.OneOf):
+                    result[field] = validator.choices[0]
+                    validatee = True
+
+        if validatee:
+            continue
+
+        if field == 'Created_By' or field == 'Modified_By':
             result[field] = user_UUID
-        
-        elif field =='Status':
+
+        elif field == 'Status':
             result[field] = 'Niet-Actief'
-        
+
         elif type(fields[field]) == MM.fields.String:
             result[field] = "Test String"
-        
+
         elif type(fields[field]) == MM.fields.UUID:
             result[field] = null_uuid
-            
+
         elif type(fields[field]) == MM.fields.Integer:
             result[field] = 42
 
         elif type(fields[field]) == MM.fields.DateTime:
             result[field] = '1992-11-23T10:00:00'
-        
+
         elif type(fields[field]) == MM.fields.Method:
             result[field] = ''
 
@@ -40,11 +45,12 @@ def generate_data(schema, user_UUID=null_uuid, excluded_prop = None):
             result[field] = []
 
         else:
-            raise NotImplementedError(f'Missing implementation for field {field} ({type(fields[field])}) with value {fields[field]}')
+            raise NotImplementedError(
+                f'Missing implementation for field {field} ({type(fields[field])}) with value {fields[field]}')
     return result
 
 
-reference_rich_beleidskeuze ={
+reference_rich_beleidskeuze = {
     "Status": "Vigerend",
     "Titel": "Beleidsbeslissing test Swen Initial",
     "Begin_Geldigheid": "2020-10-28T12:00:00",
