5b28ab67c865828165af38e8f160254b105d4b78
Swendude <swenmulderij@gmail.com>
2021-08-30 10:05:46 +0200

    Added test for search.

---
+++ Tests/test_search.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: EUPL-1.2
+# Copyright (C) 2018 - 2020 Provincie Zuid-Holland
+
+import pytest
+from werkzeug.test import Client
+from application import app
+
+@pytest.fixture
+def client():
+    """
+    Provides access to the flask test_client
+    """
+    return app.test_client()
+
+def test_fieldset(client):
+    res = client.get("v0.1/search?query=water")
+    assert(res.status_code == 200)
+    assert(list(res.get_json()[0].keys()) == ['Omschrijving','RANK', 'Titel', 'Type', 'UUID'])
