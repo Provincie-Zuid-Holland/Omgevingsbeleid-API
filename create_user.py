2dd1be47110be30413d3cc09966377477ec95d5b
Swendude <swenmulderij@gmail.com>
2022-02-15 16:24:30 +0100

    Added bcrypt to create_user

--- create_user.py
+++ create_user.py
@@ -8,13 +8,27 @@ from sqlalchemy.orm import sessionmaker
 from globals import sqlalchemy_database_uri
 from Models.gebruikers import Gebruikers_DB_Schema 
 from db import db 
+from passlib.hash import bcrypt
 
 def create_user_encrypt_pw(id, username, password, role, email):
-    new_user = Gebruikers_DB_Schema(ID=id, UUID='e583401c-e2dd-4202-81c9-9c29f24e3404', Gebruikersnaam=username, Wachtwoord=password, Rol=role, Email=email)
-    print(new_user)
+    hashed_pw = bcrypt.hash(password)
+    new_user = Gebruikers_DB_Schema(ID=id, Gebruikersnaam=username, Wachtwoord=hashed_pw, Rol=role, Email=email)
     # db.session.add(new_user)
     # all_users = db.session.query(Gebruikers_DB_Schema).all()
     # for row in all_users:
     #     print(row['Gebruikersnaam'])
     # db.session.commit()
-    
+
+# This one works (without the app context)
+def _create_user_encrypt_pw(id, username, password, role, email):
+    engine = create_engine(sqlalchemy_database_uri)
+    Session = sessionmaker(bind=engine)
+    with Session() as session:
+        hashed_pw = bcrypt.hash(password)
+        new_user = Gebruikers_DB_Schema(ID=id, Gebruikersnaam=username, Wachtwoord=hashed_pw, Rol=role, Email=email)
+        session.add(new_user)
+        session.commit()
+
+# docker-compose exec api python create_user.py 1 alex lol test alex@pzh.nl
+if __name__ == '__main__':
+    _create_user_encrypt_pw(*sys.argv[1:])
