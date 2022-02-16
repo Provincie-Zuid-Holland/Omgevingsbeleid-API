8221c7abf33455d07256adfa058f51910506f1b3
Jordy Moos <jordymoos@gmail.com>
2022-02-16 17:01:44 +0100

    updated the renamed gebruikers model

--- create_user.py
+++ create_user.py
@@ -6,13 +6,13 @@ import string
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from globals import sqlalchemy_database_uri
-from Models.gebruikers import Gebruikers_DB_Schema 
+from Models.gebruikers import Gebruikers 
 from db import db 
 from passlib.hash import bcrypt
 
 def create_user_encrypt_pw(id, username, password, role, email):
     hashed_pw = bcrypt.hash(password)
-    new_user = Gebruikers_DB_Schema(ID=id, Gebruikersnaam=username, Wachtwoord=hashed_pw, Rol=role, Email=email)
+    new_user = Gebruikers(ID=id, Gebruikersnaam=username, Wachtwoord=hashed_pw, Rol=role, Email=email)
     # db.session.add(new_user)
     # all_users = db.session.query(Gebruikers_DB_Schema).all()
     # for row in all_users:
@@ -25,7 +25,7 @@ def _create_user_encrypt_pw(id, username, password, role, email):
     Session = sessionmaker(bind=engine)
     with Session() as session:
         hashed_pw = bcrypt.hash(password)
-        new_user = Gebruikers_DB_Schema(ID=id, Gebruikersnaam=username, Wachtwoord=hashed_pw, Rol=role, Email=email)
+        new_user = Gebruikers(ID=id, Gebruikersnaam=username, Wachtwoord=hashed_pw, Rol=role, Email=email)
         session.add(new_user)
         session.commit()
 
