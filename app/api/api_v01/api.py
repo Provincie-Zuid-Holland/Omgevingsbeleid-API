e9e3709f898053271720e6647fca9f4e44a69627
awiseguy <arvinwijsman@gmail.com>
2022-08-03 16:01:16 +0200

    route for belangen endpoint

--- app/api/api_v01/api.py
+++ app/api/api_v01/api.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.api.api_v01.endpoints import (
     ambities,
+    belangen,
     beleidsmodules,
     beleidsprestaties,
     login,
@@ -18,6 +19,7 @@ from app.api.api_v01.endpoints import (
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(ambities.router, tags=["ambities"])
+api_router.include_router(belangen.router, tags=["belangen"])
 api_router.include_router(beleidskeuzes.router, tags=["beleidskeuzes"])
 api_router.include_router(beleidsdoelen.router, tags=["beleidsdoelen"])
 api_router.include_router(beleidsmodules.router, tags=["beleidsmodules"])
