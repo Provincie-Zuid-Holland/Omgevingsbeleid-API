1b7f9a4013f7c20f839023a9e29c42d19bf57936
awiseguy <arvin@wise-it.info>
2023-01-17 13:47:58 +0800

    openapi spec url for swagger-io

--- main.py
+++ main.py
@@ -17,14 +17,16 @@ settings = get_settings()
 
 def create_app() -> FastAPI:
     app = FastAPI(
-        title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V01_STR}/openapi.json"
+        title=settings.PROJECT_NAME, 
+        openapi_url=f"{settings.API_V01_STR}/spec"
     )
 
     # Set all CORS enabled origins
     if settings.BACKEND_CORS_ORIGINS:
+        origins = [str(origin) for origin in settings.BACKEND_CORS_ORIGINS]
         app.add_middleware(
             CORSMiddleware,
-            allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+            allow_origins=origins,
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
