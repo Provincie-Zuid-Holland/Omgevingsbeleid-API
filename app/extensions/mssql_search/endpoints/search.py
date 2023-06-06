94abb3a0c633c5c0ee74e31a035bc2bad423c3c2
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2023-06-06 16:22:31 +0200

    work on mssql search

--- app/extensions/mssql_search/endpoints/search.py
+++ app/extensions/mssql_search/endpoints/search.py
@@ -1,27 +1,40 @@
-from typing import List
+from typing import List, Optional
 import uuid
+
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel, validator
-from sqlalchemy import desc, select
+from sqlalchemy import text
 from sqlalchemy.orm import Session
+from bs4 import BeautifulSoup
+
 from app.core.dependencies import depends_db
 from app.dynamic.db.objects_table import ObjectsTable
 from app.dynamic.dependencies import depends_pagination
-
 from app.dynamic.endpoints.endpoint import EndpointResolver, Endpoint
 from app.dynamic.config.models import Api, EndpointConfig
 from app.dynamic.utils.pagination import Pagination
 from app.dynamic.event_dispatcher import EventDispatcher
 from app.dynamic.models_resolver import ModelsResolver
 from app.dynamic.converter import Converter
+from app.extensions.modules.db.module_objects_tables import ModuleObjectsTable
+from app.extensions.modules.db.tables import ModuleTable
+from app.extensions.users.db.tables import UsersTable
+from app.extensions.users.dependencies import depends_current_active_user
+
+
+class SearchConfig(BaseModel):
+    searchable_columns_high: List[str]
+    searchable_columns_low: List[str]
 
 
 class SearchObject(BaseModel):
+    Module_ID: Optional[int]
+    UUID: uuid.UUID
     Object_Type: str
     Object_ID: int
-    UUID: uuid.UUID
     Title: str
     Description: str
+    Score: float
 
     @validator("Title", "Description", pre=True)
     def default_empty_string(cls, v):
@@ -33,64 +46,187 @@ class SearchObject(BaseModel):
 
 class SearchResponse(BaseModel):
     Objects: List[SearchObject]
+    Total: int
 
 
 class EndpointHandler:
     def __init__(
         self,
         db: Session,
+        search_config: SearchConfig,
         pagination: Pagination,
         query: str,
     ):
         self._db: Session = db
+        self._search_config: SearchConfig = search_config
         self._pagination: Pagination = pagination
         self._query: str = query
 
     def handle(self) -> SearchResponse:
-        stmt = self._search_stmt()
-        table_rows = self._db.execute(stmt).all()
-        search_objects: List[SearchObject] = [
-            SearchObject.parse_obj(r._asdict()) for r in table_rows
-        ]
+        if not len(self._query):
+            raise ValueError("Missing search query")
+        if self._pagination.get_limit() > 50:
+            raise ValueError("Pagination limit is too high")
+
+        stmt = text(
+            f"""
+                WITH valid_uuids (Module_ID, UUID, Object_Type, Object_ID, Title, Description)
+                AS
+                (
+                    SELECT
+                        0 AS Module_ID,
+                        UUID,
+                        Object_Type,
+                        Object_ID,
+                        Title,
+                        Description
+                    FROM (
+                        SELECT
+                            UUID,
+                            Object_Type,
+                            Object_ID,
+                            Title,
+                            Description,
+                            End_Validity,
+                            ROW_NUMBER() OVER (
+                                PARTITION BY
+                                    Code
+                                ORDER BY
+                                    Modified_Date DESC
+                            ) AS _RowNumber
+                        FROM
+                            {ObjectsTable.__table__}
+                        WHERE
+                            Start_Validity <= GETDATE()
+                    ) AS valid_subquery
+                    WHERE
+                        _RowNumber = 1
+                        AND (End_Validity > GETDATE() OR End_Validity IS NULL)
+                    
+                    UNION
+
+                    SELECT
+                        Module_ID,
+                        UUID,
+                        Object_Type,
+                        Object_ID,
+                        Title,
+                        Description
+                    FROM (
+                        SELECT
+                            mo.Module_ID,
+                            mo.UUID,
+                            mo.Object_Type,
+                            mo.Object_ID,
+                            mo.Title,
+                            mo.Description,
+                            ROW_NUMBER() OVER (
+                                PARTITION BY
+                                    mo.Code
+                                ORDER BY
+                                    mo.Modified_Date DESC
+                            ) AS _RowNumber
+                        FROM
+                            {ModuleObjectsTable.__table__} AS mo
+                            INNER JOIN {ModuleTable.__table__} AS m ON mo.Module_ID = m.Module_ID
+                        WHERE
+                            m.Closed = 0
+                    ) AS module_subquery
+                    WHERE
+                        _RowNumber = 1
+                )
+
+                SELECT
+                    v.Module_ID,
+                    v.UUID,
+                    v.Object_Type,
+                    v.Object_ID,
+                    v.Title,
+                    v.Description,
+                    s.WeightedRank AS _Rank,
+                    COUNT(*) OVER() AS _Total_Count
+                FROM valid_uuids AS v
+                INNER JOIN
+                (
+                    SELECT
+                        [KEY],
+                        SUM(Rank) as WeightedRank
+                    FROM
+                    (
+                        SELECT Rank * 1 as Rank, [KEY] from CONTAINSTABLE({ObjectsTable.__table__}, ({", ".join(self._search_config.searchable_columns_high)}), :query)
+                            UNION
+                        SELECT Rank * 0.5 as Rank, [KEY] from CONTAINSTABLE({ObjectsTable.__table__}, ({", ".join(self._search_config.searchable_columns_low)}), :query)
+                            UNION
+
+                        SELECT Rank * 1 as Rank, [KEY] from CONTAINSTABLE({ModuleObjectsTable.__table__}, ({", ".join(self._search_config.searchable_columns_high)}), :query)
+                            UNION
+                        SELECT Rank * 0.5 as Rank, [KEY] from CONTAINSTABLE({ModuleObjectsTable.__table__}, ({", ".join(self._search_config.searchable_columns_low)}), :query)
+                    ) AS x
+                    GROUP BY [KEY]
+                ) AS s ON s.[KEY] = v.UUID
+                ORDER BY
+                    s.WeightedRank DESC
+                OFFSET
+                    :offset ROWS 
+                FETCH
+                    NEXT :limit ROWS ONLY
+            """
+        )
+
+        stmt = stmt.bindparams(
+            query=self._query,
+            offset=self._pagination.get_offset(),
+            limit=self._pagination.get_limit(),
+        )
+
+        results = self._db.execute(stmt)
+        search_objects: List[SearchObject] = []
+        total_count: int = 0
+
+        for row in results:
+            row = row._asdict()
+
+            description: str = ""
+            if row["Description"]:
+                try:
+                    soup = BeautifulSoup(row["Description"], "html.parser")
+                    description = soup.get_text()
+                except:
+                    pass
+
+            search_object: SearchObject = SearchObject(
+                Module_ID=row["Module_ID"] if row["Module_ID"] else None,
+                UUID=row["UUID"],
+                Object_Type=row["Object_Type"],
+                Object_ID=row["Object_ID"],
+                Title=row["Title"],
+                Description=description,
+                Score=row["_Rank"],
+            )
+            search_objects.append(search_object)
+            total_count = row["_Total_Count"]
 
         return SearchResponse(
             Objects=search_objects,
+            Total=total_count,
         )
-
-    def _search_stmt(self):
-        like_query = f"%{self._query}%"
-        stmt = (
-            select(
-                ObjectsTable.Object_Type,
-                ObjectsTable.Object_ID,
-                ObjectsTable.UUID,
-                ObjectsTable.Title,
-                ObjectsTable.Description,
-            )
-            .select_from(ObjectsTable)
-            .filter(
-                ObjectsTable.Title.like(like_query)
-                | ObjectsTable.Description.like(like_query)
-            )
-            .order_by(desc(ObjectsTable.Modified_Date))
-            .limit(self._pagination.get_limit())
-            .offset(self._pagination.get_offset())
-        )
-        return stmt
 
 
 class MssqlSearchEndpoint(Endpoint):
-    def __init__(self, path: str):
+    def __init__(self, path: str, search_config: SearchConfig):
         self._path: str = path
+        self._search_config: SearchConfig = search_config
 
     def register(self, router: APIRouter) -> APIRouter:
         def fastapi_handler(
             query: str,
             db: Session = Depends(depends_db),
             pagination: Pagination = Depends(depends_pagination),
+            user: UsersTable = Depends(depends_current_active_user),
         ) -> SearchResponse:
             handler: EndpointHandler = EndpointHandler(
                 db,
+                self._search_config,
                 pagination,
                 query,
             )
@@ -99,7 +235,7 @@ class MssqlSearchEndpoint(Endpoint):
         router.add_api_route(
             self._path,
             fastapi_handler,
-            methods=["GET"],
+            methods=["POST"],
             response_model=SearchResponse,
             summary=f"Search for objects",
             description=None,
@@ -124,6 +260,9 @@ class MssqlSearchEndpointResolver(EndpointResolver):
         resolver_config: dict = endpoint_config.resolver_data
         path: str = endpoint_config.prefix + resolver_config.get("path", "")
 
+        search_config: SearchConfig = SearchConfig(**resolver_config)
+
         return MssqlSearchEndpoint(
             path=path,
+            search_config=search_config,
         )
--- app/extensions/mssql_search/endpoints/valid_search.py
+++ app/extensions/mssql_search/endpoints/valid_search.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel, validator
 from sqlalchemy import text
 from sqlalchemy.orm import Session
+from bs4 import BeautifulSoup
 
 from app.core.dependencies import depends_db
 from app.dynamic.db.objects_table import ObjectsTable
@@ -17,12 +18,18 @@ from app.dynamic.models_resolver import ModelsResolver
 from app.dynamic.converter import Converter
 
 
-class SearchObject(BaseModel):
+class ValidSearchConfig(BaseModel):
+    searchable_columns_high: List[str]
+    searchable_columns_low: List[str]
+
+
+class ValidSearchObject(BaseModel):
+    UUID: uuid.UUID
     Object_Type: str
     Object_ID: int
-    UUID: uuid.UUID
     Title: str
     Description: str
+    Score: float
 
     @validator("Title", "Description", pre=True)
     def default_empty_string(cls, v):
@@ -32,24 +39,29 @@ class SearchObject(BaseModel):
         validate_assignment = True
 
 
-class SearchResponse(BaseModel):
-    Objects: List[SearchObject]
+class ValidSearchResponse(BaseModel):
+    Objects: List[ValidSearchObject]
+    Total: int
 
 
 class EndpointHandler:
     def __init__(
         self,
         db: Session,
+        search_config: ValidSearchConfig,
         pagination: Pagination,
         query: str,
     ):
         self._db: Session = db
+        self._search_config: ValidSearchConfig = search_config
         self._pagination: Pagination = pagination
         self._query: str = query
 
-    def handle(self) -> SearchResponse:
+    def handle(self) -> ValidSearchResponse:
         if not len(self._query):
             raise ValueError("Missing search query")
+        if self._pagination.get_limit() > 50:
+            raise ValueError("Pagination limit is too high")
 
         stmt = text(
             f"""
@@ -92,7 +104,8 @@ class EndpointHandler:
                     v.Object_ID,
                     v.Title,
                     v.Description,
-                    CAST(FLOOR(s.WeightedRank) AS INT) AS _Rank
+                    s.WeightedRank AS _Rank,
+                    COUNT(*) OVER() AS _Total_Count
                 FROM valid_uuids AS v
                 INNER JOIN
                 (
@@ -101,44 +114,73 @@ class EndpointHandler:
                         SUM(Rank) as WeightedRank
                     FROM
                     (
-                        SELECT Rank * 1 as Rank, [KEY] from CONTAINSTABLE({ObjectsTable.__table__}, (Title), :query)
+                        SELECT Rank * 1 as Rank, [KEY] from CONTAINSTABLE({ObjectsTable.__table__}, ({", ".join(self._search_config.searchable_columns_high)}), :query)
                             UNION
-                        SELECT Rank * 0.5 as Rank, [KEY] from CONTAINSTABLE({ObjectsTable.__table__}, (Description), :query)
+                        SELECT Rank * 0.5 as Rank, [KEY] from CONTAINSTABLE({ObjectsTable.__table__}, ({", ".join(self._search_config.searchable_columns_low)}), :query)
                     ) AS x
                     GROUP BY [KEY]
                 ) AS s ON s.[KEY] = v.UUID
                 ORDER BY
                     s.WeightedRank DESC
                 OFFSET
-                    0 ROWS 
+                    :offset ROWS 
                 FETCH
-                    NEXT 10 ROWS ONLY
+                    NEXT :limit ROWS ONLY
             """
         )
 
-        # stmt = self._search_stmt()
-        # table_rows = self._db.execute(stmt).all()
-        # search_objects: List[SearchObject] = [
-        #     SearchObject.parse_obj(r._asdict()) for r in table_rows
-        # ]
+        stmt = stmt.bindparams(
+            query=self._query,
+            offset=self._pagination.get_offset(),
+            limit=self._pagination.get_limit(),
+        )
 
-        return SearchResponse(
-            Objects=[],
+        results = self._db.execute(stmt)
+        search_objects: List[ValidSearchObject] = []
+        total_count: int = 0
+
+        for row in results:
+            row = row._asdict()
+
+            description: str = ""
+            if row["Description"]:
+                try:
+                    soup = BeautifulSoup(row["Description"], "html.parser")
+                    description = soup.get_text()
+                except:
+                    pass
+
+            search_object: ValidSearchObject = ValidSearchObject(
+                UUID=row["UUID"],
+                Object_Type=row["Object_Type"],
+                Object_ID=row["Object_ID"],
+                Title=row["Title"],
+                Description=description,
+                Score=row["_Rank"],
+            )
+            search_objects.append(search_object)
+            total_count = row["_Total_Count"]
+
+        return ValidSearchResponse(
+            Objects=search_objects,
+            Total=total_count,
         )
 
 
 class MssqlValidSearchEndpoint(Endpoint):
-    def __init__(self, path: str):
+    def __init__(self, path: str, search_config: ValidSearchConfig):
         self._path: str = path
+        self._search_config: ValidSearchConfig = search_config
 
     def register(self, router: APIRouter) -> APIRouter:
         def fastapi_handler(
             query: str,
             db: Session = Depends(depends_db),
             pagination: Pagination = Depends(depends_pagination),
-        ) -> SearchResponse:
+        ) -> ValidSearchResponse:
             handler: EndpointHandler = EndpointHandler(
                 db,
+                self._search_config,
                 pagination,
                 query,
             )
@@ -148,7 +190,7 @@ class MssqlValidSearchEndpoint(Endpoint):
             self._path,
             fastapi_handler,
             methods=["POST"],
-            response_model=SearchResponse,
+            response_model=ValidSearchResponse,
             summary=f"Search for valid objects",
             description=None,
             tags=["Search"],
@@ -172,6 +214,9 @@ class MssqlValidSearchEndpointResolver(EndpointResolver):
         resolver_config: dict = endpoint_config.resolver_data
         path: str = endpoint_config.prefix + resolver_config.get("path", "")
 
+        search_config: ValidSearchConfig = ValidSearchConfig(**resolver_config)
+
         return MssqlValidSearchEndpoint(
             path=path,
+            search_config=search_config,
         )
