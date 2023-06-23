7fbb03641dc488a0ec18d08afee5d27f80ce2fcb
awiseguy <arvinwijsman@gmail.com>
2023-06-23 17:50:12 +0200

    search geo request fix

--- app/extensions/werkingsgebieden/endpoints/list_objects.py
+++ app/extensions/werkingsgebieden/endpoints/list_objects.py
@@ -1,11 +1,10 @@
-from typing import List
+from typing import List, Optional
 from uuid import UUID
 
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel, conlist
+from pydantic import BaseModel, validator
 
 from app.dynamic.dependencies import depends_pagination
-from app.dynamic.utils import pagination
 
 from app.dynamic.utils.pagination import Pagination
 from app.dynamic.config.models import Api, EndpointConfig
@@ -13,24 +12,50 @@ from app.dynamic.converter import Converter
 from app.dynamic.endpoints.endpoint import Endpoint, EndpointResolver
 from app.dynamic.event_dispatcher import EventDispatcher
 from app.dynamic.models_resolver import ModelsResolver
-from app.dynamic.utils.pagination import PagedResponse, query_paginated
+from app.dynamic.utils.pagination import PagedResponse
 from app.extensions.users.db.tables import UsersTable
 from app.extensions.users.dependencies import depends_current_active_user
 from app.extensions.werkingsgebieden.dependencies import (
     depends_werkingsgebieden_repository,
 )
-from app.extensions.werkingsgebieden.models.models import (
-    GeoSearchResult,
-    SearchResultWrapper,
-)
+from app.extensions.werkingsgebieden.models.models import GeoSearchResult
 from app.extensions.werkingsgebieden.repository.werkingsgebieden_repository import (
     WerkingsgebiedenRepository,
 )
 
 
-class AreaListSchema(BaseModel):
-    # sensible max value?
-    Area_List = conlist(UUID, min_items=1, max_items=300)
+class SearchGeoRequestData(BaseModel):
+    Object_Types: Optional[List[str]]
+    Area_List: List[UUID]
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    @validator("Area_List")
+    def valid_area_list(cls, v):
+        if len(v) < 1:
+            raise ValueError("area_list requires more than 1 uuid")
+        if len(v) > 300:
+            raise ValueError("area_list is too large, max 300 items")
+        return v
+
+    @validator("Object_Types")
+    def allowed_object_type(cls, v):
+        # TODO: Config?
+        allowed = [
+            "ambitie",
+            "beleidsdoel",
+            "beleidskeuze",
+            "beleidsregel",
+            "maatregel",
+            "gebiedsprogramma",
+        ]
+
+        for obj_type in v:
+            if obj_type not in allowed:
+                raise ValueError(f"object types allowed: {str(allowed)}")
+
+        return v
 
 
 class ListObjectsInGeoEndpoint(Endpoint):
@@ -39,7 +64,7 @@ class ListObjectsInGeoEndpoint(Endpoint):
 
     def register(self, router: APIRouter) -> APIRouter:
         def fastapi_handler(
-            object_in: AreaListSchema,
+            object_in: SearchGeoRequestData,
             pagination: Pagination = Depends(depends_pagination),
             user: UsersTable = Depends(depends_current_active_user),
             repository: WerkingsgebiedenRepository = Depends(
@@ -49,6 +74,7 @@ class ListObjectsInGeoEndpoint(Endpoint):
             return self._handler(
                 repository=repository,
                 area_list=object_in.Area_List,
+                object_types=object_in.Object_Types,
                 offset=pagination.get_offset(),
                 limit=pagination.get_limit(),
             )
@@ -71,9 +97,28 @@ class ListObjectsInGeoEndpoint(Endpoint):
         area_list: List[UUID],
         offset: int,
         limit: int,
-    ):
-        return repository.get_latest_in_area(
-            in_area=area_list, limit=limit, offset=offset
+        object_types: List[str] = None,
+    ) -> PagedResponse[GeoSearchResult]:
+        # TODO: add object_type validation
+        paginated_result = repository.get_latest_in_area(
+            in_area=area_list, object_types=object_types, limit=limit, offset=offset
+        )
+        object_list = []
+        for item in paginated_result.items:
+            search_result = GeoSearchResult(
+                Gebied=str(item.Gebied_UUID),
+                Titel=item.Title,
+                Omschrijving=item.Description,
+                Type=item.Object_Type,
+                UUID=item.UUID,
+            )
+            object_list.append(search_result)
+
+        return PagedResponse(
+            total=paginated_result.total_count,
+            limit=limit,
+            offset=offset,
+            results=object_list,
         )
 
 
--- app/extensions/werkingsgebieden/repository/werkingsgebieden_repository.py
+++ app/extensions/werkingsgebieden/repository/werkingsgebieden_repository.py
@@ -2,32 +2,36 @@ from typing import List, Optional
 from uuid import UUID
 
 from sqlalchemy import desc, select
-from sqlalchemy.orm import Session, aliased
+from sqlalchemy.orm import aliased
 from sqlalchemy.sql import func
 from app.dynamic.db.objects_table import ObjectsTable
-from app.dynamic.utils.pagination import PagedResponse, query_paginated
+from app.dynamic.utils.pagination import (
+    PaginatedQueryResult,
+    query_paginated,
+)
+from app.dynamic.repository import BaseRepository
 
 from app.extensions.werkingsgebieden.db.tables import WerkingsgebiedenTable
-from app.extensions.werkingsgebieden.models.models import (
-    GeoSearchResult,
-)
 
 
-class WerkingsgebiedenRepository:
-    def __init__(self, db: Session):
-        self._db: Session = db
-
+class WerkingsgebiedenRepository(BaseRepository):
     def get_all(self) -> List[WerkingsgebiedenTable]:
         stmt = select(WerkingsgebiedenTable).order_by(
             desc(WerkingsgebiedenTable.Modified_Date)
         )
+        return self.fetch_all(stmt)
 
-        rows: List[WerkingsgebiedenTable] = self._db.scalars(stmt).all()
-        return rows
+    def get_all_paginated(
+        self, offset: int = 0, limit: int = 20
+    ) -> PaginatedQueryResult:
+        stmt = select(WerkingsgebiedenTable).order_by(
+            desc(WerkingsgebiedenTable.Modified_Date)
+        )
+        return self.fetch_paginated(stmt, offset, limit)
 
     # TODO: Object type filter
     @staticmethod
-    def latest_objects_query(in_area: Optional[List[UUID]]):
+    def latest_objects_query(in_area: Optional[List[UUID]], object_types: List[str]):
         subq = select(
             ObjectsTable,
             func.row_number()
@@ -41,6 +45,9 @@ class WerkingsgebiedenRepository:
         if in_area:
             subq = subq.filter(ObjectsTable.Gebied_UUID.in_(in_area))
 
+        if object_types:
+            subq = subq.filter(ObjectsTable.Object_Type.in_(object_types))
+
         subq = subq.subquery()
         aliased_objects = aliased(ObjectsTable, subq)
         stmt = (
@@ -51,28 +58,15 @@ class WerkingsgebiedenRepository:
         return stmt
 
     def get_latest_in_area(
-        self, in_area: List[UUID], offset=0, limit=-1
-    ) -> PagedResponse[GeoSearchResult]:
+        self, in_area: List[UUID], object_types: List[str], offset=0, limit=-1
+    ) -> PaginatedQueryResult:
         """
         Find all latest objects matching a list of Werkingsgebied UUIDs
         """
-        query = self.latest_objects_query(in_area=in_area)
+        query = self.latest_objects_query(in_area=in_area, object_types=object_types)
 
-        table_rows, total_count = query_paginated(
+        paginated_result = query_paginated(
             query=query, session=self._db, limit=limit, offset=offset
         )
 
-        object_list = []
-        for item in table_rows:
-            search_result = GeoSearchResult(
-                Gebied=str(item.Gebied_UUID),
-                Titel=item.Title,
-                Omschrijving=item.Description,
-                Type=item.Object_Type,
-                UUID=item.UUID,
-            )
-            object_list.append(search_result)
-
-        return PagedResponse(
-            total=total_count, limit=limit, offset=offset, results=object_list
-        )
+        return paginated_result
