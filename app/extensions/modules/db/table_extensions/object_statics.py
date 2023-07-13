fcf8246f1d6c6126d25aff243c8633c9e2eb0cd3
awiseguy <arvinwijsman@gmail.com>
2023-07-13 14:30:13 +0200

    cleanup and formatting

--- app/extensions/modules/db/table_extensions/object_statics.py
+++ app/extensions/modules/db/table_extensions/object_statics.py
@@ -1,15 +1,5 @@
 from sqlalchemy import String
-from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy.orm import Session, mapped_column
-from sqlalchemy.orm.session import object_session
-
-from app.extensions.modules.repository import ModuleObjectRepository
-
-
-def get_last_module_version(self):
-    query = ModuleObjectRepository.latest_versions_query(code=self.Code)
-    db: Session = object_session(self)
-    return db.scalars(query).first()
+from sqlalchemy.orm import mapped_column
 
 
 def extend_with_attributes(table):
@@ -17,5 +7,4 @@ def extend_with_attributes(table):
     used for declaring dynamic orm properties to a given table on app init
     without placing this logic outside of the extension.
     """
-    setattr(table, "Latest_Module_Version", hybrid_property(get_last_module_version))
     setattr(table, "Cached_Title", mapped_column("Cached_Title", String(255), nullable=True))
