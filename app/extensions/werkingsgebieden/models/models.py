f8162909588e2d5b962c371fda4ec19f0990648b
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2023-09-08 10:41:36 +0200

    add validities to the responses

--- app/extensions/werkingsgebieden/models/models.py
+++ app/extensions/werkingsgebieden/models/models.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime
 from typing import List, Optional, Union
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Werkingsgebied(BaseModel):
@@ -11,6 +11,8 @@ class Werkingsgebied(BaseModel):
     Created_Date: datetime
     Modified_Date: datetime
     Title: str
+    Start_Validity: Optional[datetime] = Field(None)
+    End_Validity: Optional[datetime] = Field(None)
 
     class Config:
         orm_mode = True
