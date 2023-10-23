5976aa6e9f40ef11c72f37e7177c5a4c637d61d2
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2023-10-23 14:52:27 +0200

    search for objects by their geo relation

--- app/extensions/werkingsgebieden/models/models.py
+++ app/extensions/werkingsgebieden/models/models.py
@@ -1,5 +1,6 @@
 import uuid
 from datetime import datetime
+from enum import Enum
 from typing import List, Optional, Union
 
 from pydantic import BaseModel, Field
@@ -31,3 +32,12 @@ class GeoSearchResult(BaseModel):
 class SearchResultWrapper(BaseModel):
     total: int = 0
     results: List[GeoSearchResult] = []
+
+
+class GeometryFunctions(str, Enum):
+    # Determines if the geometry is entirely contained within the Werkingsgebied.
+    CONTAINS = "CONTAINS"
+    # It's the inverse of Contains. It determines the Werkingsgebied is entirely within the given geometry
+    WITHIN = "WITHIN"
+    # Determines in the geometry and Werkingsgebied overlap
+    OVERLAPS = "OVERLAPS"
--- app/extensions/werkingsgebieden/repository/werkingsgebieden_repository.py
+++ app/extensions/werkingsgebieden/repository/werkingsgebieden_repository.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from typing import List
 from uuid import UUID
 
-from sqlalchemy import desc, select
+from shapely import wkt
+from sqlalchemy import desc, select, text
 from sqlalchemy.orm import aliased
 from sqlalchemy.sql import func
 
@@ -10,6 +11,13 @@ from app.dynamic.db.objects_table import ObjectsTable
 from app.dynamic.repository.repository import BaseRepository
 from app.dynamic.utils.pagination import PaginatedQueryResult, SortedPagination, query_paginated
 from app.extensions.werkingsgebieden.db.tables import WerkingsgebiedenTable
+from app.extensions.werkingsgebieden.models.models import GeometryFunctions
+
+SPATIAL_FUNCTION_MAP = {
+    GeometryFunctions.CONTAINS: "STContains",
+    GeometryFunctions.WITHIN: "STWithin",
+    GeometryFunctions.OVERLAPS: "STOverlaps",
+}
 
 
 class WerkingsgebiedenRepository(BaseRepository):
@@ -44,6 +52,64 @@ class WerkingsgebiedenRepository(BaseRepository):
         if in_area:
             subq = subq.filter(ObjectsTable.Gebied_UUID.in_(in_area))
 
+        if object_types:
+            subq = subq.filter(ObjectsTable.Object_Type.in_(object_types))
+
+        subq = subq.subquery()
+        aliased_objects = aliased(ObjectsTable, subq)
+        stmt = select(aliased_objects).filter(subq.c.get("_RowNumber") == 1)
+
+        paginated_result = query_paginated(
+            query=stmt,
+            session=self._db,
+            offset=pagination.offset,
+            limit=pagination.limit,
+            sort=(getattr(subq.c, pagination.sort.column), pagination.sort.order),
+        )
+
+        return paginated_result
+
+    def get_latest_by_geometry(
+        self,
+        geometry: str,
+        function: GeometryFunctions,
+        object_types: List[str],
+        pagination: SortedPagination,
+    ) -> PaginatedQueryResult:
+        # Validating the geometry should have been done already
+        # But I do it again here because we insert it as plain text into sql.
+        # Better be safe
+        try:
+            geom = wkt.loads(geometry)
+            if geom.geom_type != "Polygon":
+                raise RuntimeError("Geometry is not a valid shape")
+        except Exception as e:
+            raise RuntimeError("Geometry is not a valid shape")
+
+        spatial_function = SPATIAL_FUNCTION_MAP[function]
+        geometry_filter = f"SHAPE.{spatial_function}(geometry::STGeomFromText('{geometry}', 28992)) = 1"
+        timepoint = datetime.utcnow()
+
+        werkingsgebieden = (
+            select(WerkingsgebiedenTable.UUID)
+            .select_from(WerkingsgebiedenTable)
+            .filter(WerkingsgebiedenTable.Start_Validity < timepoint)
+            .filter(WerkingsgebiedenTable.End_Validity > timepoint)
+            .filter(text(geometry_filter))
+            .params(polygon=geometry)
+        )
+
+        subq = select(
+            ObjectsTable,
+            func.row_number()
+            .over(
+                partition_by=ObjectsTable.Code,
+                order_by=desc(ObjectsTable.Modified_Date),
+            )
+            .label("_RowNumber"),
+        ).select_from(ObjectsTable)
+
+        subq = subq.filter(ObjectsTable.Gebied_UUID.in_(werkingsgebieden))
         if object_types:
             subq = subq.filter(ObjectsTable.Object_Type.in_(object_types))
 
