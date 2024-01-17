df5fb9ed214120d94354907c8de6ae6fee390356
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2024-01-17 15:18:55 +0100

    add shape

--- app/extensions/werkingsgebieden/db/tables.py
+++ app/extensions/werkingsgebieden/db/tables.py
@@ -1,6 +1,7 @@
 import uuid
 from datetime import datetime
 
+from sqlalchemy import BLOB
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.db.base import Base
@@ -18,6 +19,8 @@ class WerkingsgebiedenTable(Base):
     End_Validity: Mapped[datetime] = mapped_column(name="Eind_Geldigheid")
 
     Title: Mapped[str] = mapped_column(name="Werkingsgebied")
+
+    SHAPE: Mapped[bytes] = mapped_column(BLOB)
 
     def __repr__(self) -> str:
         return f"Werkingsgebieden(UUID={self.UUID!r}, Title={self.Title!r})"
