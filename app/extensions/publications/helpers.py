b07b5e7e48813c777f34bd566363f923952e041f
awiseguy <arvin@wise-it.info>
2024-02-14 16:27:43 +0800

    added local zip export helper

--- app/extensions/publications/helpers.py
+++ app/extensions/publications/helpers.py
@@ -1,4 +1,6 @@
 from datetime import date, datetime
+import os
+import shutil
 
 
 def serialize_datetime(obj):
@@ -13,3 +15,24 @@ def serialize_datetime(obj):
         return [serialize_datetime(element) for element in obj]
     else:
         return obj
+
+
+def export_zip_to_filesystem(zip_binary: bytes, zip_filename: str, path: str):
+    """
+    Store zip result in temp folder for reference / debugging
+    """
+    project_root = os.getcwd()
+    temp_dir = os.path.join(project_root, path)
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+    else:
+        # Delete existing files and folders in the directory
+        for file_name in os.listdir(temp_dir):
+            file_path = os.path.join(temp_dir, file_name)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+    os.chdir(temp_dir)
+    with open(os.path.join(os.getcwd(), zip_filename), "wb") as f:
+        f.write(zip_binary)
