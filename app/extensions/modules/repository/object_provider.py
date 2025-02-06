306bdcb93309c0fb9e1eea23c4e470730de53f3c
awiseguy <arvin@wise-it.info>
2025-02-06 15:25:21 +0800

    combined object provider query

--- app/extensions/modules/repository/object_provider.py
+++ app/extensions/modules/repository/object_provider.py
@@ -1,10 +1,10 @@
-from typing import Optional
+from typing import List, Optional
 from uuid import UUID
 
 from app.core.utils.utils import table_to_dict
 from app.dynamic.db import ObjectsTable
 from app.dynamic.repository.object_repository import ObjectRepository
-from app.extensions.modules.models.models import ModuleStatusCode
+from app.extensions.modules.models.models import ModuleStatusCode, GenericModuleObjectShort
 from app.extensions.modules.repository.module_object_repository import ModuleObjectRepository
 
 
@@ -39,3 +39,31 @@ class ObjectProvider:
         return self._module_object_repository.get_latest_filtered(
             code=valid_obj.Code, minimum_status=minimum_status, is_active=True
         )
+
+    def list_all_objects_related_to_werkingsgebied(self, werkingsgebied_code: str) -> List[GenericModuleObjectShort]:
+        """get all objects and latest version of active module objects related to a specified werkingsgebied."""
+        related_objects: List[GenericModuleObjectShort] = []
+
+        regular_objects = self._object_repository.get_all_latest_by_werkingsgebied(werkingsgebied_code)
+        related_objects.extend(
+            GenericModuleObjectShort(
+                UUID=item.UUID,
+                Object_ID=item.Object_ID, 
+                Object_Type=item.Object_Type,
+                Title=item.Title
+            ) for item in regular_objects
+        )
+
+        module_objects = self._module_object_repository.get_latest_versions_by_werkingsgebied(werkingsgebied_code)
+        related_objects.extend(
+            GenericModuleObjectShort(
+                UUID=item.module_object.UUID,
+                Object_ID=item.module_object.Object_ID,
+                Object_Type=item.module_object.Object_Type,
+                Title=item.module_object.Title,
+                Module_ID=item.module.Module_ID,
+                Module_Title=item.module.Title,
+            ) for item in module_objects
+        )
+
+        return related_objects
