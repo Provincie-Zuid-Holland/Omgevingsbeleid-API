6b44fcb1194d7ededeec9b9bd12c6e3e4b353f7c
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2025-03-04 12:26:03 +0100

    chore: Guard data (#280)

--- app/extensions/users/model.py
+++ app/extensions/users/model.py
@@ -22,8 +22,8 @@ class User(BaseModel):
 
 class UserShort(BaseModel):
     UUID: UUID
-    Rol: str
-    Gebruikersnaam: str
+    # Rol: str
+    # Gebruikersnaam: str
 
     class Config:
         orm_mode = True
