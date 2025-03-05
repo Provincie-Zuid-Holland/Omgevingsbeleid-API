77bdb74e18eb19bb07ca64acfdc0bbfb53bd2051
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2025-03-05 13:02:56 +0100

    cleanup

--- app/core/settings/dynamic_settings.py
+++ app/core/settings/dynamic_settings.py
@@ -13,7 +13,6 @@ class DynamicSettings(CoreSettings):
 def create_dynamic_settings(settings_classes: List[Type[BaseSettings]] = []) -> DynamicSettings:
     fields = {}
     for ext_cls in settings_classes:
-        # In v2 use `field.annotation` to get the field’s type instead of `outer_type_`
         fields.update({name: (field.annotation, field.default) for name, field in ext_cls.model_fields.items()})
 
     MergedSettings = create_model("MergedSettings", **fields, __base__=CoreSettings)
