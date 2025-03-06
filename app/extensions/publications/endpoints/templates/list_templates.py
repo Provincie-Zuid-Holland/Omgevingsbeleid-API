0251d4881c01dbf9cbd4884bc3240d3445aae7d4
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2025-03-06 15:43:36 +0100

    feat: Filter on document type in list templates (#286)

--- app/extensions/publications/endpoints/templates/list_templates.py
+++ app/extensions/publications/endpoints/templates/list_templates.py
@@ -8,6 +8,7 @@ from app.dynamic.endpoints.endpoint import Endpoint, EndpointResolver
 from app.dynamic.models_resolver import ModelsResolver
 from app.dynamic.utils.pagination import PagedResponse, SimplePagination
 from app.extensions.publications.dependencies import depends_publication_template_repository
+from app.extensions.publications.enums import DocumentType
 from app.extensions.publications.models import PublicationTemplate
 from app.extensions.publications.permissions import PublicationsPermissions
 from app.extensions.publications.repository.publication_template_repository import PublicationTemplateRepository
@@ -22,6 +23,7 @@ class ListPublicationTemplatesEndpoint(Endpoint):
     def register(self, router: APIRouter) -> APIRouter:
         def fastapi_handler(
             is_active: Optional[bool] = None,
+            document_type: Optional[DocumentType] = None,
             pagination: SimplePagination = Depends(depends_simple_pagination),
             user: UsersTable = Depends(
                 depends_current_active_user_with_permission_curried(
@@ -30,7 +32,7 @@ class ListPublicationTemplatesEndpoint(Endpoint):
             ),
             template_repository: PublicationTemplateRepository = Depends(depends_publication_template_repository),
         ) -> PagedResponse[PublicationTemplate]:
-            return self._handler(template_repository, is_active, pagination)
+            return self._handler(template_repository, is_active, document_type, pagination)
 
         router.add_api_route(
             self._path,
@@ -48,10 +50,12 @@ class ListPublicationTemplatesEndpoint(Endpoint):
         self,
         template_repository: PublicationTemplateRepository,
         is_active: Optional[bool],
+        document_type: Optional[DocumentType],
         pagination: SimplePagination,
     ):
         paginated_result = template_repository.get_with_filters(
             is_active=is_active,
+            document_type=document_type,
             offset=pagination.offset,
             limit=pagination.limit,
         )
