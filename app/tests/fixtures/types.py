792e85a57aebcbfb5f1172a7b0bd87b78a422ffd
Max Remkes <21196963+maxrem@users.noreply.github.com>
2025-10-16 09:41:39 +0200

    add assets, object statics

--- app/tests/fixtures/types.py
+++ app/tests/fixtures/types.py
@@ -1,15 +1,18 @@
+import base64
 import hashlib
+import json
 import os
 import uuid
 from abc import ABC, abstractmethod
 from datetime import datetime
 from enum import Enum
-from typing import Any
+from typing import Any, Optional
 
 from pydantic import BaseModel
 
 from app.api.domains.users import Security
-from app.core.tables.others import StorageFileTable
+from app.core.tables.objects import ObjectStaticsTable
+from app.core.tables.others import StorageFileTable, AssetsTable
 from app.core.tables.users import UsersTable
 
 
@@ -25,6 +28,7 @@ class FixtureContext:
 class TypeEnum(Enum):
     USER = 1
     STORAGE_FILE = 2
+    ASSET = 3
 
 
 class Factory(ABC, BaseModel):
@@ -78,3 +82,48 @@ class StorageFileFactory(Factory):
                 Created_Date=self.created_date,
                 Created_By_UUID=self.get_uuid_from_id(TypeEnum.USER, 1),
             )
+
+
+class AssetFactory(Factory):
+    id: int
+    width: int
+    height: int
+    content: str
+    content_type: str = "data:image/png;base64"
+    extension: str = "png"
+
+    def create(self) -> AssetsTable:
+        asset_bytes = base64.b64decode(self.content)
+        checksum = hashlib.sha256(asset_bytes).hexdigest()
+        lookup = checksum[0:10]
+        return AssetsTable(
+            UUID=self.get_uuid_from_id(TypeEnum.ASSET, 1),
+            Created_Date=datetime(2023, 2, 2, 2, 2, 2),
+            Created_By_UUID=self.get_uuid_from_id(TypeEnum.USER, 1),
+            Lookup=lookup,
+            Hash=checksum,
+            Meta=json.dumps(
+                {"ext": self.extension, "width": self.width, "height": self.height, "size": len(self.content)}
+            ),
+            Content=f"{self.content_type},{self.content}",
+        )
+
+
+class ObjectStaticsFactory(Factory):
+    id: int
+    object_type: str
+    owner_id: int
+    title: Optional[str] = None
+    description: Optional[str] = None
+
+    def create(self) -> ObjectStaticsTable:
+        if not self.title:
+            self.title = f"Titel van {self.object_type} {self.id}"
+
+        return ObjectStaticsTable(
+            Object_ID=self.id,
+            Object_Type=self.object_type,
+            Code=f"{self.object_type}-{self.object_type}",
+            Owner_1_UUID=self.get_uuid_from_id(TypeEnum.USER, self.owner_id),
+            Cached_Title=self.title,
+        )
