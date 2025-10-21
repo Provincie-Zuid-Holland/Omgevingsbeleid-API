54b88b1d85fa61ef836ea58613eb5ffe3fc22e86
maxrem <21196963+maxrem@users.noreply.github.com>
2025-10-21 11:34:13 +0200

    feat: add module objects to models caster (#337)
    * add ModuleObjectsToModelsParser
    * change ModuleObjectsResponse (add ObjectStatics, ModuleObjectContext and specific Model data)
    * add ModelDynamicTypeBuilder
    
    ---------
    
    Co-authored-by: Jordy Moos <4688326+JordyMoos@users.noreply.github.com>

--- app/api/domains/modules/endpoints/list_module_objects_endpoint.py
+++ app/api/domains/modules/endpoints/list_module_objects_endpoint.py
@@ -1,27 +1,54 @@
 import uuid
-from typing import Annotated, List, Optional
+from typing import Annotated, Generic, List, Optional, Dict, Sequence, Tuple, TypeVar
 
 from dependency_injector.wiring import Provide, inject
 from fastapi import Depends, Query
+from pydantic import BaseModel, ConfigDict
 from sqlalchemy.orm import Session
 
 from app.api.api_container import ApiContainer
 from app.api.dependencies import depends_db_session, depends_optional_sorted_pagination
 from app.api.domains.modules.repositories.module_object_repository import ModuleObjectRepository
-from app.api.domains.modules.types import ModuleObjectActionFull, ModuleObjectShort, ModuleStatusCode
+from app.api.domains.modules.services.module_objects_to_models_parser import ModuleObjectsToModelsParser
+from app.api.domains.modules.types import (
+    ModuleObjectActionFull,
+    ModuleObjectContextShort,
+    ModuleStatusCode,
+    ObjectStaticShort,
+)
 from app.api.domains.users.dependencies import depends_current_user
 from app.api.endpoint import BaseEndpointContext
-from app.api.utils.pagination import OptionalSortedPagination, OrderConfig, PagedResponse, Sort, SortedPagination
+from app.api.utils.pagination import (
+    OptionalSortedPagination,
+    OrderConfig,
+    PagedResponse,
+    PaginatedQueryResult,
+    Sort,
+    SortedPagination,
+)
+from app.core.tables.modules import ModuleObjectContextTable, ModuleObjectsTable
+from app.core.tables.objects import ObjectStaticsTable
 from app.core.tables.users import UsersTable
 
 
-class ModuleObjectsResponse(ModuleObjectShort):
-    Status: str
+TModel = TypeVar("TModel", bound=BaseModel)
+
+
+class ModuleObjectsResponse(BaseModel, Generic[TModel]):
+    Module_ID: int
+    Module_Latest_Status: str
+
+    ObjectStatics: ObjectStaticShort
+    Object_Type: str
+    ModuleObjectContext: ModuleObjectContextShort
+    Model: TModel
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class ListModuleObjectsEndpointContext(BaseEndpointContext):
-    object_type: str
     order_config: OrderConfig
+    model_map: Dict[str, str]
 
 
 @inject
@@ -33,6 +60,9 @@ def get_list_module_objects_endpoint(
     session: Annotated[Session, Depends(depends_db_session)],
     optional_pagination: Annotated[OptionalSortedPagination, Depends(depends_optional_sorted_pagination)],
     context: Annotated[ListModuleObjectsEndpointContext, Depends()],
+    module_objects_to_models_parser: Annotated[
+        ModuleObjectsToModelsParser, Depends(Provide[ApiContainer.module_objects_to_models_parser])
+    ],
     object_type: Optional[str] = None,
     owner_uuid: Optional[uuid.UUID] = None,
     minimum_status: Optional[ModuleStatusCode] = None,
@@ -43,7 +73,7 @@ def get_list_module_objects_endpoint(
     sort: Sort = context.order_config.get_sort(optional_pagination.sort)
     pagination: SortedPagination = optional_pagination.with_sort(sort)
 
-    paginated_result = module_object_repository.get_all_latest(
+    paginated_result: PaginatedQueryResult = module_object_repository.get_all_latest(
         session=session,
         pagination=pagination,
         only_active_modules=only_active_modules,
@@ -53,22 +83,22 @@ def get_list_module_objects_endpoint(
         title=title,
         actions=actions,
     )
+    paginated_items: Sequence[Tuple[ModuleObjectsTable, ObjectStaticsTable, ModuleObjectContextTable, str]] = (
+        paginated_result.items
+    )
 
-    rows: List[ModuleObjectsResponse] = [
-        ModuleObjectsResponse(
-            Module_ID=mo.Module_ID,
-            Status=status,
-            Object_Type=mo.Object_Type,
-            Object_ID=mo.Object_ID,
-            Code=mo.Code,
-            UUID=mo.UUID,
-            Modified_Date=mo.Modified_Date,
-            Title=mo.Title,
-            ObjectStatics=mo.ObjectStatics,
-            ModuleObjectContext=mo.ModuleObjectContext,
+    rows: List[ModuleObjectsResponse] = []
+    for object_table, object_static, module_object_context, module_status in paginated_items:
+        parsed_model: BaseModel = module_objects_to_models_parser.parse(object_table, context.model_map)
+        response = ModuleObjectsResponse(
+            Module_ID=module_object_context.Module_ID,
+            Module_Latest_Status=module_status,
+            Model=parsed_model,
+            ObjectStatics=ObjectStaticShort.model_validate(object_static),
+            ModuleObjectContext=ModuleObjectContextShort.model_validate(module_object_context),
+            Object_Type=object_table.Object_Type,
         )
-        for mo, status in paginated_result.items
-    ]
+        rows.append(response)
 
     return PagedResponse(
         total=paginated_result.total_count,
