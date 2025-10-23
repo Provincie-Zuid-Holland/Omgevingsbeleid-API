14c23d36b1c363e721c7bb0404923412073fb5fc
Max Remkes <21196963+maxrem@users.noreply.github.com>
2025-10-23 13:53:52 +0200

    revert mistake

--- app/api/domains/werkingsgebieden/repositories/mssql_area_geometry_repository.py
+++ app/api/domains/werkingsgebieden/repositories/mssql_area_geometry_repository.py
@@ -13,7 +13,7 @@ MSSQL_SPATIAL_FUNCTION_MAP = {
 
 class MssqlAreaGeometryRepository(AreaGeometryRepository):
     def _text_to_shape(self, key: str) -> str:
-        return f"geometry::STGeomFromText(CONVERT(nvarchar(max), :{key}), 28992)"
+        return f"geometry::STGeomFromText(:{key}, 28992)"
 
     def _shape_to_text(self, column: str) -> str:
         return f"{column}.STAsText()"
