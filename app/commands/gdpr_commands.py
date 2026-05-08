c2769eacd1ffbfb3cf518c2d412aa2ec7680f61d
Max Remkes <21196963+maxrem@users.noreply.github.com>
2026-05-08 15:52:41 +0200

    add type hints

--- app/commands/gdpr_commands.py
+++ app/commands/gdpr_commands.py
@@ -1,20 +1,23 @@
 import base64
 import io
 import re
-from typing import Annotated, Sequence
+from typing import Annotated, Sequence, List, Dict, Optional
 
 import click
 from PIL import Image, ExifTags, UnidentifiedImageError
+from PIL.Image import Exif
+from PIL.ImageFile import ImageFile
 from dependency_injector.wiring import inject, Provide
 
 from app.api.api_container import ApiContainer
+from app.api.domains.objects.repositories.asset_repository import AssetRepository
 from app.api.domains.others.repositories import StorageFileRepository
 from app.api.domains.others.services import PdfMetaService
+from app.api.domains.others.services.pdf_meta_service import PdfMetaReport
 from app.api.domains.publications.repository import PublicationStorageFileRepository
 from app.core.db.session import SessionFactoryType, session_scope_with_context
 from app.core.tables.others import StorageFileTable, AssetsTable
 from app.core.tables.publications import PublicationStorageFileTable
-from app.api.domains.objects.repositories.asset_repository import AssetRepository
 
 
 @click.command()
@@ -26,12 +29,12 @@ def check_pdfs(
         PublicationStorageFileRepository, Provide[ApiContainer.publication.storage_file_repository]
     ],
     pdf_meta_service: Annotated[PdfMetaService, Provide[ApiContainer.pdf_meta_service]],
-):
+) -> None:
     with session_scope_with_context(db_session_factory) as session:
         storage_files: Sequence[StorageFileTable] = storage_file_repository.get_all(session)
 
         for storage_file in storage_files:
-            report_list = pdf_meta_service.report_banned_meta(storage_file.Binary)
+            report_list: List[PdfMetaReport] = pdf_meta_service.report_banned_meta(storage_file.Binary)
             if len(report_list) <= 0:
                 continue
 
@@ -43,7 +46,7 @@ def check_pdfs(
             session
         )
         for publication_storage_file in publication_storage_files:
-            report_list = pdf_meta_service.report_banned_meta(publication_storage_file.Binary)
+            report_list: List[PdfMetaReport] = pdf_meta_service.report_banned_meta(publication_storage_file.Binary)
             if len(report_list) <= 0:
                 continue
 
@@ -59,11 +62,11 @@ def check_pdfs(
 def check_images(
     db_session_factory: Annotated[SessionFactoryType, Provide[ApiContainer.db_session_factory]],
     asset_repository: Annotated[AssetRepository, Provide[ApiContainer.asset_repository]],
-):
+) -> None:
     with session_scope_with_context(db_session_factory) as session:
         assets: Sequence[AssetsTable] = asset_repository.get_all(session)
         for asset in assets:
-            match = re.match(r"data:image/(.*?);base64,(.*)", asset.Content)
+            match: Optional[re.Match[str]] = re.match(r"data:image/(.*?);base64,(.*)", asset.Content)
             if not match:
                 print(f"Asset {asset.UUID} does not have image data")
 
@@ -71,12 +74,14 @@ def check_images(
             if mime_type not in ["png", "jpg", "jpeg"]:
                 print(f"Asset {asset.UUID} has a unknown type {mime_type}")
 
-            picture_data = base64.b64decode(base64_data)
+            picture_data: bytes = base64.b64decode(base64_data)
             try:
-                image = Image.open(io.BytesIO(picture_data))
-                exif_data = image.getexif()
+                image: ImageFile = Image.open(io.BytesIO(picture_data))
+                exif_data: Exif = image.getexif()
                 if exif_data:
-                    exif_keys = {ExifTags.TAGS.get(tag_id, tag_id): value for tag_id, value in exif_data.items()}
+                    exif_keys: Dict[str, str] = {
+                        ExifTags.TAGS.get(tag_id, tag_id): str(value) for tag_id, value in exif_data.items()
+                    }
                     print(f"Asset {asset.UUID} has exif data: {exif_keys}")
             except UnidentifiedImageError as e:
                 print(f"Error opening asset {asset.UUID}: {e}")
