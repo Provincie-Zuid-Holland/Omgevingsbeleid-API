c0050733ca2246f3998a1a0c29567ce2f2c1cb73
Max <maxturk95@gmail.com>
2026-05-20 15:04:54 +0200

    Changed pagination to the default one Changed title to a string Removed description from beleidsdoelBasic

--- app/api/domains/modules/endpoints/list_module_detail_objects_endpoint.py
+++ app/api/domains/modules/endpoints/list_module_detail_objects_endpoint.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session, aliased
 from sqlalchemy.sql import and_, or_
 
 from app.api.api_container import ApiContainer
-from app.api.dependencies import depends_db_session
+from app.api.dependencies import depends_db_session, depends_optional_sorted_pagination
 from app.api.domains.modules.dependencies import depends_module
 from app.api.domains.modules.services.module_objects_to_models_parser import ModuleObjectsToModelsParser
 from app.api.domains.modules.types import (
@@ -19,12 +19,10 @@ from app.api.domains.others.types import TModel
 from app.api.domains.users.dependencies import depends_current_user
 from app.api.endpoint import BaseEndpointContext
 from app.api.utils.pagination import (
-    OptionalSort,
     OptionalSortedPagination,
     OrderConfig,
     PagedResponse,
     Sort,
-    SortOrder,
     SortedPagination,
     query_paginated_no_scalars,
 )
@@ -48,26 +46,12 @@ class ListModuleDetailObjectsEndpointContext(BaseEndpointContext):
     model_map: Dict[str, str]
 
 
-def _depends_pagination(
-    offset: Optional[int] = None,
-    limit: Optional[int] = None,
-    sort_column: Optional[str] = None,
-    sort_order: Optional[SortOrder] = None,
-) -> OptionalSortedPagination:
-    optional_sort = OptionalSort(column=sort_column, order=sort_order)
-    return OptionalSortedPagination(
-        offset=offset,
-        limit=limit if limit is not None else 50,
-        sort=optional_sort,
-    )
-
-
 @inject
 def get_list_module_detail_objects_endpoint(
     module: Annotated[ModuleTable, Depends(depends_module)],
     user: Annotated[UsersTable, Depends(depends_current_user)],
     session: Annotated[Session, Depends(depends_db_session)],
-    optional_pagination: Annotated[OptionalSortedPagination, Depends(_depends_pagination)],
+    optional_pagination: Annotated[OptionalSortedPagination, Depends(depends_optional_sorted_pagination)],
     context: Annotated[ListModuleDetailObjectsEndpointContext, Depends()],
     module_objects_to_models_parser: Annotated[
         ModuleObjectsToModelsParser, Depends(Provide[ApiContainer.module_objects_to_models_parser])
@@ -75,11 +59,10 @@ def get_list_module_detail_objects_endpoint(
     object_type: Optional[str] = None,
     actions: Annotated[List[ModuleObjectActionFull], Query()] = [],
     mine: Annotated[bool, Query()] = False,
-    query: Optional[str] = None,
+    title: Optional[str] = None,
 ) -> PagedResponse[ModuleDetailObjectsResponse]:
     sort: Sort = context.order_config.get_sort(optional_pagination.sort)
     pagination: SortedPagination = optional_pagination.with_sort(sort)
-
 
     subq = (
         select(
@@ -103,7 +86,7 @@ def get_list_module_detail_objects_endpoint(
     filters = [
         ModuleObjectsTable.Object_Type == object_type if object_type is not None else None,
         ModuleObjectContextTable.Action.in_(actions) if actions else None,
-        ModuleObjectsTable.Title.like(f"%{query}%") if query is not None else None,
+        ModuleObjectsTable.Title.like(title) if title is not None else None,
         (
             or_(
                 ObjectStaticsTable.Owner_1_UUID == user.UUID,
@@ -143,8 +126,6 @@ def get_list_module_detail_objects_endpoint(
     rows: List[ModuleDetailObjectsResponse] = []
     for object_table, object_static, module_object_context in paginated_result.items:
         parsed_model: BaseModel = module_objects_to_models_parser.parse(object_table, context.model_map)
-        if object_table.Object_Type == "beleidsdoel":
-            parsed_model.__dict__.pop("Description", None)
         response: ModuleDetailObjectsResponse = ModuleDetailObjectsResponse(
             Module_ID=module.Module_ID,
             Model=parsed_model,
