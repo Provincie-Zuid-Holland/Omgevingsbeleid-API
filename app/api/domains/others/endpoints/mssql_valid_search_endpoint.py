dcf70dc8b2d2b0df5a161ce030878253d9f2e7d1
Max <maxturk95@gmail.com>
2026-07-10 12:15:05 +0200

    Fix search character validation to allow accented letters

--- app/api/domains/others/endpoints/mssql_valid_search_endpoint.py
+++ app/api/domains/others/endpoints/mssql_valid_search_endpoint.py
@@ -1,4 +1,3 @@
-import json
 from typing import Annotated, List, Optional, Dict
 
 from bs4 import BeautifulSoup
@@ -39,7 +38,7 @@ class EndpointHandler:
     def handle(self) -> PagedResponse[ValidSearchObject]:
         if not len(self._query):
             raise ValueError("Missing search query")
-        if "\\" in json.dumps(self._query):
+        if '"' in self._query or "\\" in self._query:
             raise ValueError("Invalid search characters")
         if self._pagination.limit > 50:
             raise ValueError("Pagination limit is too high")
