b1f67d1af347c328c87c5806c519390eecbcd0e6
Max <maxturk95@gmail.com>
2026-07-13 21:50:54 +0200

    made the file download endpoint public. Changed the test to match it

--- app/api/domains/others/endpoints/files_download_endpoint.py
+++ app/api/domains/others/endpoints/files_download_endpoint.py
@@ -3,14 +3,11 @@ from typing import Annotated
 from fastapi import Depends, Response
 
 from app.api.domains.others.dependencies import depends_storage_file
-from app.api.domains.users.dependencies import depends_current_user
 from app.core.tables.others import StorageFileTable
-from app.core.tables.users import UsersTable
 
 
 def get_files_download_endpoint(
     storage_file: Annotated[StorageFileTable, Depends(depends_storage_file)],
-    _: Annotated[UsersTable, Depends(depends_current_user)],
 ) -> Response:
     filename = storage_file.Filename
     content = storage_file.Binary
