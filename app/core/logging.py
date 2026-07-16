ca5028ffc6f5a4d22259952dbbb65d4e2e2e01ce
Max Remkes <21196963+maxrem@users.noreply.github.com>
2026-07-16 12:51:51 +0200

    feat: AB#37943 install and use azure-monitor-opentelemetry

--- app/core/logging.py
+++ app/core/logging.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+from azure.monitor.opentelemetry import configure_azure_monitor
+
 
 def init_logging() -> None:
     level_str: str = os.getenv("LOG_LEVEL", "INFO").upper()
@@ -10,3 +12,11 @@ def init_logging() -> None:
         level=level,
         format="%(asctime)s %(levelname)s %(name)s: %(message)s",
     )
+
+    logger_name: str = os.getenv("LOG_LOGGER_NAME", "obzh")
+    logger = logging.getLogger(logger_name)
+
+    applicationinsights_connection_string: str = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING", "")
+    logger.info(f"applicationinsights_connection_string: {applicationinsights_connection_string}")
+    if applicationinsights_connection_string:
+        configure_azure_monitor(logger_name=logger_name)
