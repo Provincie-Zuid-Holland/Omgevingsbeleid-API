91f2f2871a937fe9d3a3ac26f203e40c54df1b34
Max Remkes <21196963+maxrem@users.noreply.github.com>
2026-07-17 12:44:03 +0200

    remove logger

--- app/core/logging.py
+++ app/core/logging.py
@@ -14,9 +14,7 @@ def init_logging() -> None:
     )
 
     logger_name: str = os.getenv("LOG_LOGGER_NAME", "obzh")
-    logger = logging.getLogger(logger_name)
 
     applicationinsights_connection_string: str = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING", "")
-    logger.info(f"applicationinsights_connection_string: {applicationinsights_connection_string}")
     if applicationinsights_connection_string:
         configure_azure_monitor(logger_name=logger_name)
