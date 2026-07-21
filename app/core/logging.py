0e22e9d9a9b58ef762d357733cd0d105d458b1fa
Max Remkes <21196963+maxrem@users.noreply.github.com>
2026-07-21 15:47:25 +0200

    feat: add log_message, add catch all exception handler

--- app/core/logging.py
+++ app/core/logging.py
@@ -1,7 +1,12 @@
 import logging
 import os
+from typing import Optional
 
 from azure.monitor.opentelemetry import configure_azure_monitor
+from fastapi import Request
+
+logger_name: str = os.getenv("LOG_LOGGER_NAME", "obzh")
+logger = logging.getLogger(logger_name)
 
 
 def init_logging() -> None:
@@ -13,8 +18,29 @@ def init_logging() -> None:
         format="%(asctime)s %(levelname)s %(name)s: %(message)s",
     )
 
-    logger_name: str = os.getenv("LOG_LOGGER_NAME", "obzh")
-
     applicationinsights_connection_string: str = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING", "")
     if applicationinsights_connection_string:
         configure_azure_monitor(logger_name=logger_name, instrumentation_options={"fastapi": {"enabled": True}})
+
+
+def log_message(
+    message: str, severity: int = logging.INFO, exception: Optional[Exception] = None, request: Optional[Request] = None
+) -> None:
+    api_env = os.getenv("API_ENV", "unknown environment")
+    extra = {"api_env": api_env}
+    if request:
+        extra["request_path"] = request.url.path
+        extra["request_method"] = request.method
+
+    match severity:
+        case s if s >= logging.ERROR:
+            if exception:
+                logger.exception(message, exc_info=exception, extra=extra)
+            else:
+                logger.error(message, extra=extra)
+        case s if s >= logging.WARNING:
+            logger.warning(message, extra=extra)
+        case s if s >= logging.INFO:
+            logger.info(message, extra=extra)
+        case _:
+            logger.debug(message, extra=extra)
