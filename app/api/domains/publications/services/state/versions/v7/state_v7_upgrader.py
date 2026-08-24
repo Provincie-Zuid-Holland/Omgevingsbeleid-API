98ece5bfbae9e2eac4d8794551f8df7dace840f1
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2026-08-24 16:23:09 +0200

    fix string formatting

--- app/api/domains/publications/services/state/versions/v7/state_v7_upgrader.py
+++ app/api/domains/publications/services/state/versions/v7/state_v7_upgrader.py
@@ -147,4 +147,4 @@ class ActTextAssetParser:
         if match:
             return match.group(1)
 
-        raise RuntimeError("Unable to find asset uuid in the name: '{name}'")
+        raise RuntimeError(f"Unable to find asset uuid in the name: '{name}'")
