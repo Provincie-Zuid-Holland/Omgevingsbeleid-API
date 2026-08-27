e8fd2c3d8437e03d3582d6d8b0570df65cdfe8aa
Max Remkes <21196963+maxrem@users.noreply.github.com>
2026-08-27 16:16:42 +0200

    add password guard, enable/disable constraints

--- app/commands/database_commands.py
+++ app/commands/database_commands.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 import click
 from dependency_injector.wiring import Provide, inject
-from sqlalchemy import Engine, event, text
+from sqlalchemy import Engine, text
 from sqlalchemy.orm import Session
 
 from app.api.api_container import ApiContainer
@@ -10,13 +10,33 @@ from app.core.db import table_metadata
 from tests.fixtures.internal.fixtures_service import FixturesService
 
 
+def _guard_non_dev_environment(db_password: str) -> None:
+    if db_password != "Passw0rd":
+        raise RuntimeError("Database connection not in DEV environment")
+
+
+def _disable_foreign_keys(dialect: str, session: Session) -> None:
+    if dialect == "sqlite":
+        session.execute(text("PRAGMA foreign_keys = OFF"))
+    elif dialect == "mssql":
+        session.execute(text("""EXEC sp_MSforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT ALL";"""))
+
+
+def _enable_foreign_keys(dialect: str, session: Session) -> None:
+    if dialect == "sqlite":
+        session.execute(text("PRAGMA foreign_keys = OFF"))
+    elif dialect == "mssql":
+        session.execute(text("""EXEC sp_MSforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT ALL";"""))
+
+
 @click.command()
 @inject
-def initdb(db_engine: Annotated[Engine, Provide[ApiContainer.db_engine]]):
-    click.echo("Initialized the database")
-    if db_engine.dialect.name != "sqlite":
-        if not click.confirm("Database is not SQLite. Do you want to continue?"):
-            return
+def initdb(
+    db_password: Annotated[str, Provide[ApiContainer.config.DB_PASS]],
+    db_engine: Annotated[Engine, Provide[ApiContainer.db_engine]],
+):
+    _guard_non_dev_environment(db_password)
+    click.echo("Initializing the database")
 
     table_metadata.create_all(db_engine)
     click.echo("Done")
@@ -24,11 +44,15 @@ def initdb(db_engine: Annotated[Engine, Provide[ApiContainer.db_engine]]):
 
 @click.command()
 @inject
-def dropdb(db_engine: Annotated[Engine, Provide[ApiContainer.db_engine]]):
+def dropdb(
+    db_password: Annotated[str, Provide[ApiContainer.config.DB_PASS]],
+    db_engine: Annotated[Engine, Provide[ApiContainer.db_engine]],
+):
+    _guard_non_dev_environment(db_password)
     click.echo("Dropping database")
-    if db_engine.dialect.name != "sqlite":
-        if not click.confirm("Database is not SQLite. Do you want to continue?"):
-            return
+
+    with Session(db_engine) as session:
+        _disable_foreign_keys(db_engine.dialect.name, session)
 
     table_metadata.drop_all(db_engine)
     click.echo("Dropped the database")
@@ -36,26 +60,20 @@ def dropdb(db_engine: Annotated[Engine, Provide[ApiContainer.db_engine]]):
 
 @click.command()
 @inject
-def load_fixtures(db_engine: Annotated[Engine, Provide[ApiContainer.db_engine]]):
+def load_fixtures(
+    db_password: Annotated[str, Provide[ApiContainer.config.DB_PASS]],
+    db_engine: Annotated[Engine, Provide[ApiContainer.db_engine]],
+):
+    _guard_non_dev_environment(db_password)
     click.echo("Loading fixtures")
-    if db_engine.dialect.name != "sqlite":
-        if not click.confirm("Database is not SQLite. Do you want to continue?"):
-            return
 
-    def _disable_foreign_keys(dbapi_connection, _record) -> None:
-        if db_engine.dialect.name == "sqlite":
-            dbapi_connection.execute("PRAGMA foreign_keys = OFF")
-        elif db_engine.dialect.name == "mssql":
-            dbapi_connection.execute("""EXEC sp_MSforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT ALL";""")
-
-    event.listen(db_engine, "connect", _disable_foreign_keys)
     try:
         with Session(db_engine) as session:
+            _disable_foreign_keys(db_engine.dialect.name, session)
+
             FixturesService().load(session)
             session.commit()
 
-            if db_engine.dialect.name == "mssql":
-                session.execute(text("""EXEC sp_MSforeachtable "ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL";"""))
+            _enable_foreign_keys(db_engine.dialect.name, session)
     finally:
-        event.remove(db_engine, "connect", _disable_foreign_keys)
-    click.echo("Done")
+        click.echo("Done")
