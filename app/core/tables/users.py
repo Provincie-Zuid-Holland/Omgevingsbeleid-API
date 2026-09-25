bca097456db5ce3f559dad0c634395d5b9d40ab8
Max Remkes <21196963+maxrem@users.noreply.github.com>
2026-09-25 14:03:48 +0200

    rename user_roles fields

--- app/core/tables/users.py
+++ app/core/tables/users.py
@@ -20,11 +20,11 @@ class UsersTable(Base, SerializerMixin):
     Status: Mapped[str | None]
 
     user_roles: Mapped[list["UserRoleTable"]] = relationship(
-        back_populates="User",
+        back_populates="user",
         cascade="all, delete-orphan",
     )
     Roles: AssociationProxy[list[str]] = association_proxy(
-        "user_roles", "Role", creator=lambda role: UserRoleTable(Role=role)
+        "user_roles", "role", creator=lambda role: UserRoleTable(role=role)
     )
 
     # @todo: move to separate table
@@ -43,11 +43,10 @@ class UsersTable(Base, SerializerMixin):
         return data
 
 
-# The only doubt I have is the language of the table name and column names. Since the Gebruikers table is in Dutch but the rest is English
 class UserRoleTable(Base):
     __tablename__ = "user_roles"
 
-    User_UUID: Mapped[uuid.UUID] = mapped_column(ForeignKey("Gebruikers.UUID"), primary_key=True)
-    Role: Mapped[str] = mapped_column(String(100), primary_key=True)
+    user_uuid: Mapped[uuid.UUID] = mapped_column(ForeignKey("Gebruikers.UUID"), primary_key=True)
+    role: Mapped[str] = mapped_column(String(100), primary_key=True)
 
-    User: Mapped["UsersTable"] = relationship(back_populates="user_roles")
+    user: Mapped["UsersTable"] = relationship(back_populates="user_roles")
